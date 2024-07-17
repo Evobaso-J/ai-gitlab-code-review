34769900124fc412799c3ccb3ead22b85280e853
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2024-07-17 11:47:38 +0200

    feat: extend error class

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -1,20 +1,26 @@
+import type { FastifyError } from "fastify";
+
 export class BaseError<T extends string> extends Error {
     override name: T;
     override message: string;
     override cause: any;
+    statusCode: FastifyError['statusCode'];
 
     constructor({
         name,
         message,
         cause,
+        statusCode = 500,
     }: {
         name: T;
         message: string;
         cause?: any;
+        statusCode?: FastifyError['statusCode'];
     }) {
         super();
         this.name = name;
         this.message = message;
         this.cause = cause;
+        this.statusCode = statusCode;
     }
 }
